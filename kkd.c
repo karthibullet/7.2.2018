fe9a74fd5bfa1c17d8b9b84d9deb23fb49cafb9a
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-02-07 16:41:18 +0530

    kkd.c

---
+++ kkd.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,i;
+printf("Enter all the even numbers");
+scanf("%d",&a)
+printf("The even numbers are");
+i=0;
+while(i<n)
+{
+if(i%2)
+}
+printf("All the even numbeers are:");
+getch();
+return 0;
+}
