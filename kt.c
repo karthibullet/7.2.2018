fd369e6421cdc3ae2ef77a480f80bf397b3f81b8
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-02-07 16:01:27 +0530

    kt.c

---
+++ kt.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+int num,reverse num;
+printf("enter the number");
+scanf("%d",&number);
+if(num=reverse num)
+{
+printf("\nthe number is palindrome");
+else
+printf("\n the number is not palindrome");
+}
+  return 0;
+}
